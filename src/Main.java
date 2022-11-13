1dd7cca2f6b33d3da171bebec911ad60381c59f0
Jappananny <Jappananny@gmail.com>
2022-11-13 18:42:28 +0300

    ДЗ 2.3/1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import transport.Car;
+import transport.Train;
+import transport.Transport;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Transport.Key keyLada = new Car.Key(false,false);
+        Car.Insurance insuranceLada = new Transport.Insurance(1995, 5500,"АЯ8990780");
+        Car[] car = new Car[5];
+        car[0] = new Car("Lada", "", 2015, "", "желтый", 1.7,
+                false,"Седан","М655AS32",3,   false,keyLada,insuranceLada);
+        Transport.Key keyTrain = new Train.Key(false,false);
+        Car.Insurance insuranceTrain = new Transport.Insurance(2035, 1235500,"АE8097011");
+        Train[] train = new Train[5];
+        train[0] = new Train("Ласточка","B-901","Россия",2011,
+                keyTrain,insuranceTrain, 3500, 301, "Белорусский вокзал", "Минск-Пассажирский", 11);
+        train[1] = new Train("Ленинград","D-125","Россия",2019,
+                keyTrain,insuranceTrain, 1700, 270, "Ленинградский вокзал", "Ленинград-Пассажирский", 8);
+        //Вывод результата
+        System.out.println("================================================================");
+        //System.out.println(car[0].toString());
+        transport.Car.printAllCar(car);
+        System.out.println("================================================================");
+        transport.Train.printAllTrain(train);
     }
 }
---
+++ src/transport/Car.java
@@ -0,0 +1,110 @@
+package transport;
+import lombok.Getter;
+import static java.lang.Character.isDigit;
+@Getter
+public class Car extends Transport{
+    private String color;;
+    private double engineVolume;
+    private String transmission;
+    private final String bodyType;
+    private String typeTyre;
+    private String regNumber;
+    private final int seats;
+
+
+
+    public Car(String brand, String model, int productionYear, String productionCountry, String color,
+               double engineVolume, boolean transmission, String bodyType, String regNumber, int seats,
+               boolean typeTyre, Key key, Insurance insurance){
+        super(brand, model, productionCountry, productionYear, key, insurance);
+        boolean regNumberCorrect = false;
+        if (color.length() <= 0 || color == null){
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (engineVolume <= 0){
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (seats <= 0){
+            this.seats = 2;
+        } else {
+            this.seats = seats;
+        }
+        if(regNumber.length() == 9 && regNumber != null) {
+            for (int i = 0; i < regNumber.length(); i++){
+                switch (i) {
+                    case 0 : regNumberCorrect = !isDigit(regNumber.toCharArray()[i]);
+                        break;
+                    case 1 :
+                    case 2 :
+                    case 3 : regNumberCorrect = isDigit(regNumber.toCharArray()[i]);
+                        break;
+                    case 4 :
+                    case 5 : regNumberCorrect = !isDigit(regNumber.toCharArray()[i]);
+                        break;
+                    case 6 :
+                    case 7 :
+                    case 8 : regNumberCorrect = isDigit(regNumber.toCharArray()[i]);
+                        break;
+                }
+                if(!regNumberCorrect){
+                    break;
+                }
+            }
+        }
+        this.regNumber = regNumberCorrect ? regNumber : "Некоректный номер";
+        this.transmission = transmission ? "МКП" : "АКП";
+        if (bodyType.length() <= 0 || bodyType == null){
+            this.bodyType = "default";
+        } else {
+            this.bodyType = bodyType;
+        }
+        this.typeTyre = typeTyre ? "летняя" : "зимняя";
+
+
+    }
+
+    public Car setColor(String color) {
+        this.color = color;
+        return this;
+    }
+    public Car setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+        return this;
+    }
+    public Car setTransmission(String transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+    public Car setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+        return this;
+    }
+
+    public Car setTypeTyre(String typeTyre) {
+        this.typeTyre = typeTyre;
+        return this;
+    }
+    @Override
+    public String toString() {
+        return "Бренд: " + getBrand() + " Модель: " + getModel() + ". Год выпуска: " + getProductionYear() +
+                ". Сборка: " + getProductionCountry() + ". Цвет " + color + ". Объем двигателя - "
+                + engineVolume + " литра. " + "\nТип коробки передач: " + transmission + ". Тип кузова: "
+                + bodyType + ". Регистрационный номер автомобиля: " + regNumber + ".\nКоличество мест: "
+                + seats + ". На автомобиле установленна " + typeTyre + " резина. " + getKey() + ". \n"
+                + getInsurance() + " рублей.";
+    }
+    public static void printAllCar(Car[] car) {
+        for (Car car1 : car) {
+            if (car1 != null) {
+                System.out.println(car1.toString());
+            }
+        }
+    }
+
+
+}
+
